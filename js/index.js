ca5c6fa982a2ed95bd85d22ad860a88d2407d926
benchiz <trigubov.art@mail.ru>
2023-10-04 12:31:43 +0300

    need grid

--- js/index.js
+++ js/index.js
@@ -1,18 +1,15 @@
 "use strict";
 
 let popup = document.querySelector('.js-modal');
-let popupBackground = document.querySelector('.js-modal__wrapper');
 let openPopup = document.querySelector('.js-open-popup');
 let closePopup = document.querySelector('.js-close-popup');
 
 openPopup.addEventListener('click', (e) => {
   e.preventDefault();
-  popupBackground.classList.add('active');
-  popup.classList.add('active');
+  popup.classList.add('js-active');
 });
 
 closePopup.addEventListener('click', (e) => {
   e.preventDefault();
-  popupBackground.classList.remove('active');
-  popup.classList.remove('active');
+  popup.classList.remove('js-active');
 });
