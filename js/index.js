9bdae2792f3943a1c8645c774e96bfb0064d9981
benchiz <trigubov.art@mail.ru>
2023-10-25 15:47:56 +0300

    fix error event in modal 25/10/23

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-let popup = document.querySelector('.js-modal');
-let openPopup = document.querySelector('.js-open-popup');
-let closePopup = document.querySelector('.js-close-popup');
+const popup = document.querySelector('.js-modal');
+const openPopup = document.querySelector('.js-open-popup');
+const closePopup = document.querySelector('.js-close-popup');
+const formPopup = popup.querySelector('.contacts__form');
+const nameInput = popup.querySelector('[name=name]');
+const emailInput = popup.querySelector('[name=email]');
 
 openPopup.addEventListener('click', (e) => {
   e.preventDefault();
@@ -12,4 +15,23 @@ closePopup.addEventListener('click', (e) => {
   e.preventDefault();
   popup.classList.remove('js-active');
   document.querySelector('html').classList.remove('is-disabled-scroll');
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode === 27) {
+    if (popup.classList.contains('js-active')) {
+      e.preventDefault();
+      popup.classList.remove('js-active');
+      document.querySelector('html').classList.remove('is-disabled-scroll');
+    }
+  }
+});
+
+formPopup.addEventListener('submit', (e) => {
+  if (!nameInput.value || !emailInput.value) {
+    e.preventDefault();
+    popup.classList.remove('js-error');
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add('js-error');
+  }
 });
